a8481c0ef746c44ebf3b77e2e7c3ae5177762bfd
Weijia Huang <hhuangweijia@gmail.com>
2025-11-23 14:31:49 -0500

    Initial Commit

---
+++ LingCode/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LingCode
+//
+//  Created by Weijia Huang on 11/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LingCode/LingCodeApp.swift
@@ -0,0 +1,32 @@
+//
+//  LingCodeApp.swift
+//  LingCode
+//
+//  Created by Weijia Huang on 11/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LingCodeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
